e48b648cbd0a41771048c9261a09a207db1f1052
re han <rehanakbar342@gmail.com>
2024-09-03 19:55:27 +0800

    Add project files.

---
+++ 01Systems-Dot Net Engineer Test/GUID/CustomGUID.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Systems_Dot_Net_Engineer_Test.GUID
+{
+    public static class CustomGUID
+    {
+        public static string GenerateGuid()
+        {
+            byte[] randomBytes = new byte[16]; // 16 bytes = 128 bits
+            using (RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create())
+            {
+                randomNumberGenerator.GetBytes(randomBytes);
+            }
+
+            // Convert bytes to hexadecimal string
+            StringBuilder hex = new StringBuilder(32);
+            foreach (byte b in randomBytes)
+            {
+                // Convert each byte to 2 hex characters
+                hex.Append(b.ToString("x2")); 
+            }
+
+            // Format the GUID in the standard 8-4-4-4-12 format
+            return $"{hex.ToString(0, 8)}" +
+                $"-{hex.ToString(8, 4)}-" +
+                $"{hex.ToString(12, 4)}-" +
+                $"{hex.ToString(16, 4)}-" +
+                $"{hex.ToString(20, 12)}";
+        }
+    }
+}
---
+++ 01Systems-Dot Net Engineer Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using _01Systems_Dot_Net_Engineer_Test.GUID;
+
+
+Console.WriteLine("-------Generate an Identifier that is Globaly Unique --------\n");
+
+
+// Generat the Guid by using Custom 
+string customGuid = CustomGUID.GenerateGuid();
+Console.WriteLine("Generated GUID: " + customGuid);
+
+
+
+
+Console.ReadLine();
+
